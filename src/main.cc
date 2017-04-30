c4a16d258ef58abc8ea602c83946a278b2a11401
Bert <maxcooncat75@gmail.com>
2017-04-30 23:32:53 +0300

    Add files via upload

--- src/main.cc
+++ src/main.cc
@@ -1,1 +1,13 @@
+#include <node.h>
+#include <crfsuite.hpp>
+#include "trainer_class.h"
+#include "tagger_class.h"
+
+void Initialize(v8::Local<v8::Object> exports) {  
+  TrainerClass::Init(exports);
+  TaggerClass::Init(exports);
+}
+
+
+NODE_MODULE(module_name, Initialize)  
 
